d65fdf7112e58906a0e5793919e0ee40ac1abd45
MarleySetwaba <73256020+MarleySetwaba@users.noreply.github.com>
2022-05-27 03:09:36 +0200

    Laravel Marblog Part 1 Commit

--- marblog/database/seeders/DatabaseSeeder.php
+++ marblog/database/seeders/DatabaseSeeder.php
@@ -4,7 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\User;
+use App\Models\Post;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,11 +15,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        //USE USER MODEL TO A CREATE A USER
+        $user1 = User::create([
+            'name' => 'Marley',
+            'email' => 'marley@gmail.com',
+            'password' => 'dieavirgin123'
+        ]);
+       //USE POST MODEL TO CREATE A POST
+        $post1 = Post::create([
+            'user_id' => $user1->id,
+            'title' => 'My Favorite Song RN',
+            'content' => 'Gus Dapperton - Moodna, Once With Grace'
+        ]);
+       
     }
 }
